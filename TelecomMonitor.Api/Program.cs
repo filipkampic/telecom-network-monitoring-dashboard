d50a1b19b476df9455947e85b7115b71003ceb3b
Filip Kampić <filipkampic1@gmail.com>
2026-02-21 22:37:43 +0100

    Add FluentValidation and CreateEventRequestValidator for event input validation

--- TelecomMonitor.Api/Program.cs
+++ TelecomMonitor.Api/Program.cs
@@ -1,4 +1,6 @@
+using FluentValidation;
 using Microsoft.EntityFrameworkCore;
+using TelecomMonitor.Api.Validators;
 using TelecomMonitor.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +12,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
+builder.Services.AddValidatorsFromAssemblyContaining<CreateEventRequestValidator>();
 
 var app = builder.Build();
 
---
+++ TelecomMonitor.Api/Validators/CreateEventRequestValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using TelecomMonitor.Api.DTOs;
+
+namespace TelecomMonitor.Api.Validators
+{
+    public class CreateEventRequestValidator : AbstractValidator<CreateEventRequest>
+    {
+        public CreateEventRequestValidator() {
+
+            RuleFor(x => x.DeviceId)
+                .NotEmpty().WithMessage("DeviceId is required.");
+
+            RuleFor(x => x.Severity)
+                .NotEmpty().WithMessage("Severity is required.")
+                .Must(s => new[] { "Info", "Warning", "Error" }.Contains(s)).WithMessage("Severity must be Info, Warning, or Error.");
+
+            RuleFor(x => x.Message)
+                .NotEmpty().WithMessage("Message is required.");
+
+            RuleFor(x => x.Timestamp)
+                .LessThanOrEqualTo(DateTime.UtcNow.AddMinutes(1)).WithMessage("Timestamp cannot be in the future.");
+        }
+    }
+}
